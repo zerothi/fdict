690ac71afc76e0be945bb61ffb6266d80aaca383
Nick Papior Andersen <nickpapior@gmail.com>
2014-05-25 20:52:49 +0000

    Added log to the fvar library.
    Added new test to dictionary collection. Sadly when doing:
      call routine('hello'.kv.'value')
    it will create a data leakage due to the pointer nature of the data.
    Hence, one should always do it with a designated variable and call delete on that object.

---
+++ test/tst_dict_mem3.f90
@@ -0,0 +1,49 @@
+program tests
+
+  use tst_utils
+
+  implicit none
+
+  real(dp) :: va(400,400) ! roughly 1.22 MB
+  
+  integer :: i, N, step
+
+  N = 500
+  step = 25
+
+  write(*,*)'Dictionary passing to routine (in sub-call)'
+  do i = 1 , N
+     call after_mem
+     if ( mod(i,step) == 0 ) then
+        call show_mem
+     end if
+  end do
+  
+  write(*,*)'Dictionary passing to routine'
+  do i = 1 , N
+     call mem('hello'.kv.va)
+     if ( mod(i,step) == 0 ) then
+        call show_mem
+     end if
+  end do
+
+contains
+
+  subroutine after_mem()
+    type(dict) :: dic
+    dic = 'hello'.kv.va
+    call mem(dic)
+  end subroutine after_mem
+    
+  subroutine mem(dic)
+    type(dict), intent(in) :: dic
+
+    integer :: i, N
+    ! do nothing with the dictionary, check that it gets deleted
+    N = 1
+    do i = 1 , 10
+       N = N + i
+    end do
+  end subroutine mem
+
+end program tests
