8f16e42ae14224a0f9e7fa638efbd17015d8606a
Nick Papior <nickpapior@gmail.com>
2016-07-28 16:23:17 +0200

    travis: macos
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Do pre-install commands
-if [ $TRAVIS_OS_NAME == "osx" ]; then
+if [ "x$TRAVIS_OS_NAME" == "xosx" ]; then
     brew update
 else
     sudo apt-get install -qq gfortran
