3b749f376148ba9814f3571be0ed1269be34c3e0
Nick Papior <nickpapior@gmail.com>
2016-07-28 16:21:31 +0200

    travis: macos
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,9 +3,6 @@
 # Do pre-install commands
 if [ $TRAVIS_OS_NAME == "osx" ]; then
     brew update
-    if ! `which gfortran` ; then
-        brew install gcc
-    fi
 else
     sudo apt-get install -qq gfortran
 fi
