801500d2673d245d1186b2f6e80ff4635935a2b0
Nick Papior <nickpapior@gmail.com>
2016-07-28 16:24:23 +0200

    travis: adding more information in build
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Run installation
+echo "CPP version"
+cpp --version
+echo "gfortran version"
+gfortran --version
+# Show help from Makefile
+make show
+# Actually build fdict
 make
