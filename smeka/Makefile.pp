72664ba6dbaeabe46b7246e74828a9f572157152
Nick Papior <nickpapior@gmail.com>
2016-07-29 14:11:49 +0200

    build: smeka move file
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- smeka/Makefile.pp
+++ smeka/Makefile.pp
@@ -1,9 +1,10 @@
-_SMEKA_cpp = 1
+_SMEKA_pp = 1
 
 # This smeka file creates the preprocessor
 # settings.
 
-# Default pre-processor commands
+# Default pre-processor commands are defined in
+#    Makefile.default
 # (probably we should do a check to ensure
 #  it works)
 # Sadly there are many variations of CPP
@@ -11,38 +12,21 @@ _SMEKA_cpp = 1
 #   -nostdinc is needed to not include standard includes (which are C comments)
 #   -E only preprocess, do not try and compile
 #   -P do not add line markers
-# clang-3.5 (MacOSx): -E -EP -xc
+# clang-3.5 (MacOSx): -E -xc
 # clang-3.7> (MacOSx): -E -P -x c
 # cpp(gnu): -E -P -x c -nostdinc
-CPP ?= cpp -E -P -x c
-CPPFLAGS ?=
-FPPFLAGS ?=
 
 
-ifeq ($(uname_S),Linux)
-endif
-ifeq ($(uname_S),Darwin)
-endif
-ifeq ($(uname_S),SunOS)
-endif
-ifeq ($(uname_S),FreeBSD)
-endif
-ifeq ($(uname_S),OpenBSD)
-endif
-ifeq ($(uname_S),NetBSD)
-endif
-ifeq ($(uname_S),Cygwin)
-endif
-
-
-.PHONY: smeka-show-cpp
-.NOTPARALLEL: smeka-show-cpp
-smeka-show-cpp:
+.PHONY: smeka-show-pp
+.NOTPARALLEL: smeka-show-pp
+smeka-show-pp:
 	@echo "  CPP      = $(CPP)"
 	@echo "  CPPFLAGS = $(CPPFLAGS)"
+	@echo "  FPP      = $(FPP)"
 	@echo "  FPPFLAGS = $(FPPFLAGS)"
 	@echo "  INCLUDES = $(INCLUDES)"
-show: smeka-show-cpp
+
+show: smeka-show-pp
 
 # Local Variables:
 #  mode: makefile-gmake
