54111c3777c8c94f1f60b68be52675fc5f1c388e
Nick Papior <nickpapior@gmail.com>
2018-11-27 09:56:54 +0100

    bug: final fix for LHS re-allocation
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- src/variable.sh
+++ src/variable.sh
@@ -126,7 +126,7 @@ for v in ${vars[@]} ; do
 	_psnl "if ( this%t == '$v$d' ) then"
 	_psnl "p$v${d}_1%p = p$v${d}_2%p"
 	_psnl "allocate(this%enc(size(transfer(p$v${d}_1, local_enc_type))))"
-	_psnl "this%enc = transfer(p$v${d}_1, local_enc_type)"
+	_psnl "this%enc(:) = transfer(p$v${d}_1, local_enc_type)"
 	[ $d -lt $(var_N $v) ] && _ps "else"
     done
     _psnl "endif"
